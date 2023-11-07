a6560fd7b1fa4cf50a68109cbc80e65ad115269e
olegKrainyk <oleg.kr944@gmail.com>
2023-11-06 20:53:24 -0500

    numberOfCars
    output number of cars from server

--- server/main.py
+++ server/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
 from PIL import Image
 from ultralytics import YOLO
+# Load a pretrained YOLO model (recommended for training)
+model = YOLO('yolov8n.pt')
 
 
 app = Flask(__name__)
@@ -8,14 +10,18 @@ app = Flask(__name__)
 @app.route('/api/process_image', methods=['GET'])
 def process_image():
     try:
-        # Load a pretrained YOLO model (recommended for training)
-        model = YOLO('yolov8n.pt')
+
+        numberCars = 0;
         results = model(source="source/northTOeast0.png", conf=0.3, save=True)
 
-        log = results[0].tojson()
+        for result in results:
+            boxes = result.boxes.cpu().numpy()
+            for box in boxes:
+                if (result.names[int(box.cls[0])] == 'car') or (result.names[int(box.cls[0])] == 'truck'):
+                    numberCars += 1
 
         with open("cars.txt", "wt") as file:
-            file.write(str(log))
+            file.write(str(numberCars))
 
         with open("cars.txt", "rt") as file:
             file_content = file.read()
