583a704dd24adf422fb8d6a10ff83fa3254e3b5a
Roman L <49673221+HyxerPyth@users.noreply.github.com>
2023-11-06 21:38:38 -0500

    pohui commit
    pohui commit

--- server/main.py
+++ server/main.py
@@ -1,6 +1,20 @@
 from flask import Flask, jsonify
-from PIL import Image
+import cv2
 from ultralytics import YOLO
+
+# Opens a image in RGB mode
+def crop_image():
+    img = cv2.imread("source/northTOeast0.png")
+
+    # Define the coordinates and dimensions for cropping
+    x = 1730  # X-coordinate of the top-left corner
+    y = 1730  # Y-coordinate of the top-left corner
+    w = 1741  # Width of the cropped region
+    h = 1200  # Height of the cropped region
+
+    crop_img = img[y:y + h, x:x + w]
+    save_path = "source/crop_images/image1.png"
+    cv2.imwrite(save_path, crop_img)
 
 
 app = Flask(__name__)
@@ -10,7 +24,8 @@ def process_image():
     try:
         # Load a pretrained YOLO model (recommended for training)
         model = YOLO('yolov8n.pt')
-        results = model(source="source/northTOeast0.png", conf=0.3, save=True)
+        crop_image()
+        results = model(source="source/crop_images/image1.png", conf=0.3, save=True)
 
         log = results[0].tojson()
 
