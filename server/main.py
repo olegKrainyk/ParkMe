fb3ba214d17cb024694e432d21dc5dc3b92a635e
olegKrainyk <oleg.kr944@gmail.com>
2023-11-06 20:42:13 -0500

    working v0.01

--- server/main.py
+++ server/main.py
@@ -1,28 +1,13 @@
-""" from PIL import Image
+from flask import Flask, jsonify
+from PIL import Image
 from ultralytics import YOLO
 
-
-# Load a pretrained YOLO model (recommended for training)
-model = YOLO('yolov8n.pt')
-results = model(source="source/northTOeast0.png", conf=0.3, save=True)
-
-file_path = "cars.txt"
-
-log = results[0].tojson()
-
-with open(file_path, "wt") as file:
-    file.write(str(log)) """
-
-from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/api/process_image', methods=['GET'])
 def process_image():
     try:
-        from PIL import Image
-        from ultralytics import YOLO
-
         # Load a pretrained YOLO model (recommended for training)
         model = YOLO('yolov8n.pt')
         results = model(source="source/northTOeast0.png", conf=0.3, save=True)
@@ -42,4 +27,4 @@ def process_image():
         return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
