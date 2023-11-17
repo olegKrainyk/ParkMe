3e3ee84cb13ccec739c2c50c26074800dbb9bd18
olegKrainyk <oleg.kr944@gmail.com>
2023-11-17 13:31:45 -0500

    main component created

--- client/react-client/src/App.js
+++ client/react-client/src/App.js
@@ -1,9 +1,15 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Main from './Pages/Main/Main';
 
 export default function App() {
   return (
     <div className="App">
-     
+      <Routes>
+
+        <Route path="/main" element={<Main />} />
+
+      </Routes>
     </div>
   );
 }
---
+++ client/react-client/src/Pages/Main/Main.jsx
@@ -0,0 +1,11 @@
+
+import './Main.css'
+
+export default function Main() {
+  return (
+    <div className="main-page">
+
+        mainn
+    </div>
+  );
+}
