531e1644eea78198b98fd683aad978ef84bd2d75
olegKrainyk <oleg.kr944@gmail.com>
2023-11-17 14:15:30 -0500

    motiondiv button

--- client/react-client/src/Pages/Main/Main.jsx
+++ client/react-client/src/Pages/Main/Main.jsx
@@ -1,11 +1,15 @@
-
+import {motion} from 'framer-motion'
 import './Main.css'
+import transition from '../../transition/transition'
 
-export default function Main() {
+function Main() {
   return (
     <div className="main-page">
-
-        mainn
+        <motion.div className='main-button icon' animate={{ transform: ['translateY(0px)', 'translateY(-20px)', 'translateY(0px)'] }}transition={{ease: 'easeIn' ,repeat: Infinity, duration: 1.5 }}>
+            find spot
+        </motion.div>
     </div>
   );
 }
+
+export default transition(Main);
